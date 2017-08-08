30d63278a4f7c41b5ad6a49c3ab9ad9b9a225565
Petr Bělohlávek <petrbel@users.noreply.github.com>
2017-08-08 16:43:43 +0200

    echo

--- git-utils.sh
+++ git-utils.sh
@@ -7,18 +7,20 @@
 # Example:
 # fallback_clone_branch https://github.com/Cognexa/cxflow.git restore
 function fallback_clone_branch {
+    echo "Fallback clone: " "$1" "$2"
     url="$1"
     target_branch="$2"
     old_pwd=$(pwd)
     project_name=$(echo "$url" | sed 's|.*/\(.*\)\.git|\1|g')
+    echo "Project name: " "$project_name"
 
     cd /tmp
-    git clone "$1"
+    git clone "$url"
     cd "$project_name"
     project_path=$(pwd)
 
     for branch in "$target_branch" "dev" "master"; do
-        echo "Trying checkout to $2" > /dev/stderr
+        echo "Trying checkout to $target_branch" > /dev/stderr
         git checkout "$target_branch"
         if [ $? -eq 0 ]; then
             break
@@ -35,6 +37,7 @@ function fallback_clone_branch {
 # Example:
 # fallback_pip_install_branch https://github.com/Cognexa/cxflow.git restore
 function fallback_pip_install_branch {
+    echo "Fallback pip install: " "$1" "$2"
     project_path=$(fallback_clone_branch "$1" "$2")
     cd "$project_path"
     pip3 install .
