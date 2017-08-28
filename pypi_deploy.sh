1f26ae439930ceec888383d9816424afd554e7b4
Teyras <Teyras@gmail.com>
2017-08-28 15:53:52 +0200

    remove redundant checks from pypi_deploy.sh

--- pypi_deploy.sh
+++ pypi_deploy.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$CI" ]; then
-    echo "Will only continue on CI"
-    exit
-fi
-
-if [[ $CIRCLE_BRANCH != "master" ]]; then
-    echo "Will only continue for master builds"
-    exit
-fi
-
 # build package and upload to private pypi index
 echo "[distutils]" > ~/.pypirc
 echo "index-servers = pypi" >> ~/.pypirc
