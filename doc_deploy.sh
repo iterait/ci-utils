8cd94df70a7daf007d21b7892fd71924b7f089b8
Adam Blažek <adam.blazek@cognexa.com>
2018-04-01 02:39:17 +0200

    Update doc_deploy.sh

--- doc_deploy.sh
+++ doc_deploy.sh
@@ -19,6 +19,9 @@ cd docs
 
 # update the shared templates etc.
 if [ -d _base ]; then
+	cd _base
+	git pull
+	cd ../
 	git submodule update --remote _base
 	pip3 install -r _base/requirements.txt
 fi
